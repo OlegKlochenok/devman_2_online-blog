70e7e060cfa286c1353a499f2cf2f1266a73124b
OlegKlochenok <roundabout.ru@gmail.com>
2020-12-05 18:54:21 +0300

    Add post cards

--- blog/views.py
+++ blog/views.py
@@ -29,6 +29,7 @@ def index(request):
     popular_posts = all_posts.annotate(likes_count=Count('likes')).order_by('-likes_count')[:3]
     fresh_posts = all_posts.order_by('-published_at')[:5]
 
+
     context = {
         'most_popular_posts': [serialize_post(post) for post in popular_posts],
         'fresh_posts': [serialize_post(post) for post in fresh_posts],
